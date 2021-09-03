1f1b97cf5c82cb699e1c83f506ae32b751a3395f
Nic Gamble <nic.gamble@kcom.com>
2021-09-03 14:04:27 +0100

    Test for Issue 164

---
+++ LazyCache.UnitTests/Issue164Fixture.cs
@@ -0,0 +1,59 @@
+﻿namespace LazyCache.UnitTests
+{
+    using System.Collections.Generic;
+    using AutoFixture;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Issue164Fixture
+    {
+        public class MyClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void Issue164()
+        {
+            var sut = new LazyCache.CachingService();
+
+            var fixture = new AutoFixture.Fixture();
+
+            var values = fixture.Create<List<MyClass>>();
+            sut.Add("MyKey", values);
+
+
+            if (sut.TryGetValue<List<MyClass>>("MyKey", out var cachedValues))
+            {
+                cachedValues.Should().BeEquivalentTo(values);
+            }
+            else
+            {
+                Assert.Fail("Not Found");
+            }
+        }
+
+        [Test]
+        public void Issue164_NotInCache()
+        {
+            var sut = new LazyCache.CachingService();
+
+            var fixture = new AutoFixture.Fixture();
+
+            var values = fixture.Create<List<MyClass>>();
+            sut.Add("MyKey1", values);
+
+
+            if (sut.TryGetValue<List<MyClass>>("MyKey2", out var cachedValues))
+            {
+                Assert.Fail("Found??");
+            }
+            else
+            {
+                cachedValues.Should().BeNull();
+            }
+        }
+    }
+}
